8c8b5f17d42669ce09b8453065f5309ebcd13326
Alexandre Chanson <a.chanson.tours@gmail.com>
2022-02-09 11:46:32 +0100

    TP1 - solutions / ajouterArc

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,18 +1,20 @@
 import nonDir.Graphe;
+import nonDir.Noeud;
 
 import java.util.List;
+import java.util.Set;
 
 public class Algorithms {
 
-    private static <T> Graphe<T> arbreProfondeur(Graphe<T> g) {
+    private static <T> Graphe<T> arbreProfondeur(Graphe<T> g, Noeud<T> depart) {
         return null;
     }
 
-    private static <T> Graphe<T> arbreLargeur(Graphe<T> g) {
+    private static <T> Graphe<T> arbreLargeur(Graphe<T> g, Noeud<T> depart) {
         return null;
     }
 
-    private static <T> List<Graphe<T>> composantesConnexes(Graphe<T> g) {
+    private static <T> List<Set<Noeud<T>>> composantesConnexes(Graphe<T> g) {
         return null;
     }
 }
