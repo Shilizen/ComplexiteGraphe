07da1da5c514f5a6b85334078f206f68af736c7f
Alexandre Chanson <a.chanson.tours@gmail.com>
2022-02-21 10:23:11 +0100

    Update GrapheLoader.java

--- src/nonDir/GrapheLoader.java
+++ src/nonDir/GrapheLoader.java
@@ -5,8 +5,19 @@ public class GrapheLoader {
         return null; // TODO charger le fichier vers une structure Graphe du bon type
     }
 
-    public static Graphe<Integer> chargerEntier(String chemin){
-        return null; // TODO charger le fichier vers une structure Graphe du bon type
+        public static Graphe<Integer> chargerEntier(String chemin){
+        try {
+            List<String> lines =  Files.readAllLines(Paths.get(chemin));
+            Graphe<Integer> g = new GrapheImpl<>();
+            for (String l : lines){
+                String[] tmp = l.split("\t");
+                g.ajouterArc(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]));
+            }
+            return g;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static boolean enregistrer(Graphe g, String chemin){
