00a28b88c00b22775dfd7fcecf9bb0fe32b8ad26
Alexandre Chanson <a.chanson.tours@gmail.com>
2022-03-14 11:42:50 +0100

    Code for project

--- src/dir/DiGrapheLoader.java
+++ src/dir/DiGrapheLoader.java
@@ -1,13 +1,66 @@
 package dir;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class DiGrapheLoader {
-    public static DiGraphe<String> fromTSPLibFile(String path){
-        //TODO
+    public static MatriceDiGraphe<String> fromTSPLibFile(String path){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String name = br.readLine();
+            String type = br.readLine().replace("TYPE","").replace(":","").replace(" ", "");
+            String comment = br.readLine();
+            int size = Integer.parseInt(br.readLine().replace("DIMENSION", "").replace(":","").replace(" ", ""));
+            String metric = br.readLine().replace("EDGE_WEIGHT_TYPE","").replace(":","").replace(" ", "");
+
+            System.out.println(name);
+            System.out.println(comment);
+            System.out.println(type);
+            System.out.println(size);
+            System.out.println(metric);
+
+            if (metric.equals("EUC_2D")){
+                int[] ids = new int[size];
+                double[][] points = new double[size][2];
+                System.out.println(br.readLine());
+                for (int i = 0; i < size; i++) {
+                    String[] tmp = br.readLine().split(" ");
+                    ids[i] = Integer.parseInt(tmp[0]);
+                    points[i] = new double[]{Double.parseDouble(tmp[1]), Double.parseDouble(tmp[2])};
+                }
+                System.out.println(br.readLine());
+
+                // Init the graph with ids of nodes
+                MatriceDiGraphe<String> g = new MatriceDiGraphe<>(size);
+                for (int i = 0; i < size; i++) {
+                    g.getIds().add(String.valueOf(ids[i]));
+                }
+
+                //fill the distance matrix
+                double[][] adjptr = g.getAdj();
+                for (int i = 0; i < size; i++) {
+                    for (int j = i+1; j < size; j++) {
+                        adjptr[i][j] = Math.sqrt(Math.pow(points[i][0] - points[j][0],2) + Math.pow(points[i][1] - points[j][1],2));
+                        adjptr[j][i] = adjptr[i][j];
+                    }
+                }
+
+                return g;
+            }
+            else {
+                System.err.println("Unsupported file format");
+                return null;//TODO
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
--- src/dir/MatriceDiGraphe.java
+++ src/dir/MatriceDiGraphe.java
@@ -1,22 +1,46 @@
 package dir;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MatriceDiGraphe<T> implements DiGraphe<T> {
 
     private double[][] adj;
-    private T[] ids;
+    private ArrayList<T> ids;
     private int n;
 
-    private MatriceDiGraphe(){
-        //pointless
+    public MatriceDiGraphe(int size){
+        n = size;
+        ids = new ArrayList<>(n);
+        // init the array to nan everywhere (no arcs)
+        adj = new double[n][n];
+        for (int i = 0; i <n; i++)
+            Arrays.fill(adj[i], Double.NaN);
     }
 
     public MatriceDiGraphe(DiGraphe<T> source){
-        //TODO
+        n = source.compterNoeuds();
+        ids = new ArrayList<>(source.getNoeuds());
+
+        if (source instanceof ListeDiGraphe){
+            // init the array to nan everywhere (no arcs)
+            adj = new double[n][];
+            for (int i = 0; i <n; i++)
+                Arrays.fill(adj[i], Double.NaN);
+            // add the arcs replacing the NaN where they exit
+            for (Arc<T> a : source.getArcs()) {
+                adj[ids.indexOf(a.source)][ids.indexOf(a.destination)] = a.valeur;
+            }
+        }
+        else if (source instanceof MatriceDiGraphe){
+            MatriceDiGraphe<T> s = (MatriceDiGraphe<T>) source;
+            for (int i = 0; i <n; i++)
+                System.arraycopy(s.adj[i], 0, adj[i], 0, n); // We can array copy this is way faster
+        }
+        else {
+            throw new UnsupportedOperationException("Unsupported type for deep copy operation");
+        }
     }
 
     @Override
@@ -38,7 +62,7 @@ public class MatriceDiGraphe<T> implements DiGraphe<T> {
 
     @Override
     public Set<T> getNoeuds() {
-        return Stream.of(ids).collect(Collectors.toSet());
+        return new HashSet<>(ids);
     }
 
     @Override
@@ -47,7 +71,7 @@ public class MatriceDiGraphe<T> implements DiGraphe<T> {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!Double.isNaN(adj[i][j]))
-                    arcs.add(new Arc<>(ids[i], ids[j], adj[i][j]));
+                    arcs.add(new Arc<>(ids.get(i), ids.get(j), adj[i][j]));
             }
         }
         return arcs;
@@ -60,21 +84,83 @@ public class MatriceDiGraphe<T> implements DiGraphe<T> {
 
     @Override
     public boolean ajouterArc(T src, T dest, double val) {
-        throw new UnsupportedOperationException("Immutable");
+        int idx = ids.indexOf(src);
+        int idx2 = ids.indexOf(dest);
+
+        if (idx == -1)
+            throw new IllegalArgumentException("Node '"+src.toString()+"' must be in graph");
+        if (idx2 == -1)
+            throw new IllegalArgumentException("Node '"+dest.toString()+"' must be in graph");
+
+        adj[idx][idx2] = val;
+        return true;
     }
 
     @Override
     public boolean supprimerArc(T v, T w) {
-        throw new UnsupportedOperationException("Immutable");
+        int idx = ids.indexOf(v);
+        int idx2 = ids.indexOf(w);
+
+        if (idx == -1)
+            throw new IllegalArgumentException("Node '"+v.toString()+"' must be in graph");
+        if (idx2 == -1)
+            throw new IllegalArgumentException("Node '"+w.toString()+"' must be in graph");
+
+        adj[idx][idx2] = Double.NaN;
+        return true;
     }
 
     @Override
     public Set<T> getSuccesseurs(T v) {
-        return null; //TODO
+        int idx = -1;
+        for (int i = 0; i < ids.size(); i++) {
+            if (v.equals(ids.get(i))) {
+                idx = i;
+                break;
+            }
+        }
+        if (idx == -1)
+            throw new IllegalArgumentException("Node '"+v.toString()+"' must be in graph");
+        Set<T> res = new HashSet<>();
+        for (int i = 0; i < ids.size(); i++) {
+            if (!Double.isNaN(adj[idx][i]) && i != idx)
+                res.add(ids.get(i));
+        }
+        return res;
     }
 
     @Override
     public double getValeurArc(T u, T v) {
-        return 0;  //TODO
+        int idx = ids.indexOf(u);
+        int idx2 = ids.indexOf(v);
+
+        if (idx == -1)
+            throw new IllegalArgumentException("Node '"+u.toString()+"' must be in graph");
+        if (idx2 == -1)
+            throw new IllegalArgumentException("Node '"+v.toString()+"' must be in graph");
+
+        return adj[idx][idx2];
+    }
+
+    void setIds(ArrayList<T> ids) {
+        this.ids = ids;
+    }
+
+    ArrayList<T> getIds() {
+        return ids;
+    }
+
+    double[][] getAdj() {
+        return adj;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("--- MatriceDiGraphe ---").append("\n");
+        sb.append(ids).append("\n");
+        for (int i = 0; i < n; i++) {
+            sb.append(ids.get(i)).append(": ").append(Arrays.toString(adj[i])).append("\n");
+        }
+        return sb.toString();
     }
 }
